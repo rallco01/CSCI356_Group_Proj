befcc018aef5c2feda3f37f5dd90187b3ec3514e
OSCARWB <oscarwb2000@gmail.com>
2021-09-04 12:35:27 +1000

    Added physics based rotation to player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,10 +11,19 @@ public class PlayerController : MonoBehaviour
 	public float throttleResponse = 10;
 	public float rspeed = 10;
 	public float cameraHeight = 10;
-	public float rcon = 10;
-	public bool angdra = true;
+	//public float rcon = 10;
+	public bool stabass = false;
+	public float stabassAngle;
+
+	public float P=1;
+	public float I=1;
+	public float D=1;
+	private float lastAV = 0;
+
 
 	private GameObject boi;
+
+	private bool wasStabAss = false;
 
 	void Update()
 	{
@@ -72,17 +81,64 @@ public class PlayerController : MonoBehaviour
 			kpress = true;
 		}
 
+		if(Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Q))
+		{
+			if(stabass)
+			{
+				wasStabAss = true;
+				stabass = false;
+			}
+		}
+
 		if (Input.GetKey(KeyCode.E))
 		{
 			r += rspeed * t;
 			kpress = true;
 		}
 
+		if ((Input.GetKeyUp(KeyCode.E) && !Input.GetKeyDown(KeyCode.Q)) || (Input.GetKeyUp(KeyCode.Q) && !Input.GetKeyDown(KeyCode.E)))
+		{
+			if (wasStabAss)
+			{
+				wasStabAss = false;
+				stabass = true;
+				stabassAngle = transform.eulerAngles.y;
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			stabassAngle = transform.eulerAngles.y;
+			stabass = !stabass;
+		}
+
 		if (kpress)
 		{
-			rot.y += r;
 			gameObject.GetComponent<Rigidbody>().AddRelativeForce(x, 0, z);
+			rot.y += r;
 			gameObject.GetComponent<Rigidbody>().AddRelativeTorque(0, r, 0);
+		}
+		
+		if (stabass)
+		{
+			float p = -gameObject.GetComponent<Rigidbody>().angularVelocity.y;
+			float i = stabassAngle - gameObject.transform.eulerAngles.y;
+
+			if(Mathf.Abs(i) > 180)
+			{
+				i -= 180;
+				i *= -1;
+			}
+
+			//Debug.Log(i);
+
+			float d = -(gameObject.GetComponent<Rigidbody>().angularVelocity.y-lastAV) / t;
+
+			float PID = P * p + D * d + I*i;
+
+			//Debug.Log(PID);
+
+			gameObject.GetComponent<Rigidbody>().AddRelativeTorque(0, PID, 0);
 		}
 	}
 }
