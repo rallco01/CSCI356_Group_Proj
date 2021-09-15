578c331e8fa4613ffbbda2f16ae32c50953c2a14
OSCARWB <oscarwb2000@gmail.com>
2021-09-15 15:37:18 +1000

    Added shipController
    Added a ship controller class that abstracts the movements of a ship so that in the future an AI for enimy ships can use the controller for moving ships

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,32 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	private Vector3 rot;
-
-	public float thrust = 10;
-	public float throttle = 100;
-	public float throttleResponse = 10;
-	public float rspeed = 10;
-	public float cameraHeight = 10;
-	//public float rcon = 10;
-	public bool stabass = false;
-	public float stabassAngle;
-
-	public float P=1;
-	public float I=1;
-	public float D=1;
-	private float lastAV = 0;
-
-
-	private GameObject boi;
-
-	private bool wasStabAss = false;
-
+	private shipController sc = null;
 	private projectileLauncher pl = null;
 
 	private void Start()
 	{
 		pl = gameObject.GetComponentInChildren<projectileLauncher>();
+		sc = gameObject.GetComponentInChildren<shipController>();
 	}
 
 	void Update()
@@ -38,132 +19,75 @@ public class PlayerController : MonoBehaviour
 		float z = 0;
 		float x = 0;
 		float r = 0;
+		float tr = 0;
 		bool kpress = false;
 
 		if (Input.GetKey(KeyCode.LeftShift))
 		{
-			throttle += throttleResponse * t;
-			if (throttle > 100)
-			{
-				throttle = 100;
-			}
+			tr += t;
 		}
 
 		if (Input.GetKey(KeyCode.LeftControl))
 		{
-			throttle -= throttleResponse * t;
-			if(throttle < 0)
-			{
-				throttle = 0;
-			}
+			tr -= t;
 		}
 
 		if (Input.GetKey(KeyCode.W))
 		{
-			z += throttle * t * thrust;
+			z += t;
 			kpress = true;
 		}
 
 		if (Input.GetKey(KeyCode.S))
 		{
-			z -= throttle * t * thrust;
+			z -= t;
 			kpress = true;
 		}
 
 		if (Input.GetKey(KeyCode.A))
 		{
-			x -= throttle * t * thrust;
+			x -= t;
 			kpress = true;
 		}
 
 		if (Input.GetKey(KeyCode.D))
 		{
-			x += throttle * t * thrust;
+			x += t;
 			kpress = true;
 		}
 
 		if (Input.GetKey(KeyCode.Q))
 		{
-			r -= rspeed * t;
+			r -= t;
 			kpress = true;
-		}
-
-		if(Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Q))
-		{
-			if(stabass)
-			{
-				wasStabAss = true;
-				stabass = false;
-			}
 		}
 
 		if (Input.GetKey(KeyCode.E))
 		{
-			r += rspeed * t;
+			r += t;
 			kpress = true;
-		}
-
-		if ((Input.GetKeyUp(KeyCode.E) && !Input.GetKeyDown(KeyCode.Q)) || (Input.GetKeyUp(KeyCode.Q) && !Input.GetKeyDown(KeyCode.E)))
-		{
-			if (wasStabAss)
-			{
-				wasStabAss = false;
-				stabass = true;
-				stabassAngle = transform.eulerAngles.y;
-			}
 		}
 
 		if (Input.GetKeyDown(KeyCode.T))
 		{
-			stabassAngle = transform.eulerAngles.y;
-			stabass = !stabass;
+			//sc.pointAt(transform.eulerAngles);
+			//sc.stabass = !sc.stabass;
+			sc.setStabass(!sc.stabass);
 		}
 
-		if(Input.GetMouseButtonDown(1))
+		if(Input.GetMouseButtonDown(0))
 		{
-			pl.shoot();
+			//pl.shoot();
 		}
 
 		if (kpress)
 		{
 			gameObject.GetComponent<Rigidbody>().AddRelativeForce(x, 0, z);
-			rot.y += r;
-			gameObject.GetComponent<Rigidbody>().AddRelativeTorque(0, r, 0);
-		}
-
-		/* Stability Assist:
-		 * Because the rotation of the ship is controlled through physics
-		 * there needs to be a physics based way to maintain a heading.
-		 * This is done using a PID (proportional integral derivative) controll loop
-		 * to vary the direction and magnitude of a rotatial force to keep a ships heading
-		 * 
-		 *  Proportional (p): A steady heading is have a rotational velocity of 0°/s.
-		 *    This is set to be the angular velocity of the ship in the opposite direction,
-		 *    so that it applies force in the oppisite directoion of its rotation stopping it
-		 *  
-		 *  Integral (i): The intergral of velocity is position, so have a steady heading
-		 *    is having a constant position, x°. This is set to be the difference of between,
-		 *    the current heading and the desired heading.
-		 *  
-		 *  Derivative (d): The derivity of velocity is accelleration.
-		 */
-		if (stabass)
-		{
-			float p = -gameObject.GetComponent<Rigidbody>().angularVelocity.y;
-			float i = stabassAngle - gameObject.transform.eulerAngles.y;
-
-			//this is done to make sure it rotates going the shortest distance, especially when going between angle such as 300° and 10° where it crosses 0°
-			if (Mathf.Abs(i) > 180)
-			{
-				i = i % 180;
-				i *= -1;
-			}
-
-			float d = -(gameObject.GetComponent<Rigidbody>().angularVelocity.y-lastAV) / t;
-
-			float PID = P * p + D * d + I*i;
-
-			gameObject.GetComponent<Rigidbody>().AddRelativeTorque(0, PID, 0);
+			//rot.y += r;
+			//gameObject.GetComponent<Rigidbody>().AddRelativeTorque(0, r, 0);
+			sc.setThrottle(tr);
+			sc.thrustIn(new Vector3(x, 0, z));
+			sc.rotate(new Vector3(0, r, 0));
 		}
 	}
 }
